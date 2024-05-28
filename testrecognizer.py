886cff0c77d57d14bf3c8014f5f19c95276409fe
alex-valder <116109433+alex-valder@users.noreply.github.com>
2024-05-28 13:19:50 -0400

    Delete testrecognizer.py

--- testrecognizer.py
+++
@@ -1,9 +0,0 @@
-h = "the weather is great"
-words = h.split(' ')
-for i in range(len(words)):
-    if words[i] == "weather":
-        temp = words[i]
-        break
-    else:
-        temp = "not found"
-print(temp)
