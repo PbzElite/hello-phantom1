6c2a56b5a0fb623a62071ccaadef6a69672a0e44
Fred-is-now-new <116109452+Fred-is-now-new@users.noreply.github.com>
2024-12-11 09:53:08 -0500

    Update run_all.py

--- final/run_all.py
+++ final/run_all.py
@@ -3,7 +3,7 @@ import keyboard
 #imports scrape_control class to be easily called
 from scrape_control import Scraper
 
-#loop meant to simulate runtime conditions; iterates each time alt is pressed, until ` is pressed (exits loop and stops program
+#loop meant to simulate runtime conditions; iterates each time alt is pressed, until ` is pressed (exits loop and stops program)
 while not keyboard.is_pressed('`'):
     if keyboard.is_pressed('alt'):
       #code to run in loop starts here
