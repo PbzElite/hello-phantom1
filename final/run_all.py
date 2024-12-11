ef07e01510de554a58edc114143a31874e457ac1
Fred-is-now-new <116109452+Fred-is-now-new@users.noreply.github.com>
2024-12-10 22:16:23 -0500

    Create run_all.py
    Master file, will eventually import all portions of code and streamline the running process

---
+++ final/run_all.py
@@ -0,0 +1,10 @@
+import keyboard
+
+#imports scrape_control class to be easily called
+from scrape_control import Scraper
+
+#loop meant to simulate runtime conditions; iterates each time alt is pressed, until ` is pressed (exits loop and stops program
+while not keyboard.is_pressed('`'):
+    if keyboard.is_pressed('alt'):
+      #code to run in loop starts here
+        print(Scraper.scrapeControl('weather', 'na'))
